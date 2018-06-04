3fae11146bc7a5d089e9edd8ebeb7ccf9a9372c4
AleksReen <aleksandr.reentovich@tut.by>
2018-06-04 23:32:26 +0300

    Added SelectionPage, EmpoyeesPage

---
+++ WpfApp/Pages/SelectedPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for SelectedPage.xaml
+    /// </summary>
+    public partial class SelectedPage : Page
+    {
+        public SelectedPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEmployee_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("Pages/Employees.xaml", UriKind.Relative));
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
