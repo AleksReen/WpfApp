722fedc17b2efe210c16ddffa4edf58885975e0d
AleksReen <aleksandr.reentovich@tut.by>
2018-06-06 00:20:04 +0300

    Added DB, EF

--- WpfApp/Pages/Employees.xaml.cs
+++ WpfApp/Pages/Employees.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp.EDMX;
 
 namespace WpfApp.Pages
 {
@@ -20,9 +22,29 @@ namespace WpfApp.Pages
     /// </summary>
     public partial class Employees : Page
     {
+        public static EmpDBEntities db { get; set; }
+        private List<Employee> ListEmployees;
+
         public Employees()
         {
+            db = new EmpDBEntities();
             InitializeComponent();
+            ListEmployees = new List<Employee>();
+        }
+
+        private void EmployeesPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            ListEmployees = db.Employee.ToList();
+            this.DataGridEmployee.ItemsSource = ListEmployees;
+            tbCount.Text = ListEmployees.Count().ToString();
+            tbDate.Text = DateTime.Today.ToString("dd MMMM yyyy");
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            DataGridEmployee.IsReadOnly = false;
+            DataGridEmployee.BeginEdit();
+            tbSt.Text = "РЕДАКТИРУЕТСЯ";
         }
     }
 }
---
+++ WpfApp/StaticResources/ListTitle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WpfApp.EDMX;
+using WpfApp.Pages;
+
+namespace WpfApp.StaticResources
+{
+    public class ListTitle:List<Title>
+    {
+        public ListTitle() => this.AddRange(Employees.db.Title.Select(x => x).OrderBy(x => x.Title1).ToArray());
+    }
+}
